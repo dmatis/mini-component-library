6f08e7bdb9635b25b3c7a0c8630c4ae12c8b5e0d
Darren Matis-Mei <dmatismei@microsoft.com>
2022-08-18 22:42:22 -0700

    Initial attempt at progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,25 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const ProgressBarWrapper = styled.strong`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  width: 370px;
+  height: 24px;
+  display: block;
+`;
+
+const ProgressBarInner = styled.div`
+  background-color: ${COLORS.primary};
+  height: 16px;
+  width: ${p => (p.value/100) * 370}px;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <ProgressBarWrapper size={size}>
+      <ProgressBarInner value={value} size={size} />
+    </ProgressBarWrapper>
+  );
 };
 
 export default ProgressBar;
