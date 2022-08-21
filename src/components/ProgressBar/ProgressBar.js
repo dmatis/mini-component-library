12db92ed4d0944149b2bc0312e99552cc0d2ae90
Darren Matis-Mei <dmatismei@microsoft.com>
2022-08-20 17:23:56 -0700

    ProgressBar mostly working

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,23 +5,46 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBarWrapper = styled.strong`
+const ProgressBarWrapper = styled.div`
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   width: 370px;
-  height: 24px;
-  display: block;
+  height: auto;
+  position: relative;
+  border-radius: 4px;
 `;
 
 const ProgressBarInner = styled.div`
   background-color: ${COLORS.primary};
-  height: 16px;
+  height: var(--height);
+  padding: var(--padding);
+  position: relative;
   width: ${p => (p.value/100) * 370}px;
+  border-radius: var(--borderRadius) ${p => (p.value >= 98 ? `var(--borderRadius)` : 0)} ${p => (p.value >= 98 ? `var(--borderRadius)` : 0)} var(--borderRadius);
+
 `;
 
+const SIZES = {
+  small: {
+    "--height": 8 + "px",
+    "--borderRadius": 4 + "px",
+  },
+  medium: {
+    "--height": 12 + "px",
+    "--borderRadius": 4 + "px",
+  },
+  large: {
+    "--height": 24 + "px",
+    "--borderRadius": 8 + "px",
+    "--padding": 4 + "px"
+  }
+};
+
 const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+  console.log({styles});
   return (
-    <ProgressBarWrapper size={size}>
-      <ProgressBarInner value={value} size={size} />
+    <ProgressBarWrapper style={styles}>
+      <ProgressBarInner value={value} style={styles} />
     </ProgressBarWrapper>
   );
 };
