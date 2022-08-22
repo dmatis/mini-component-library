d284e63b76ae9ffc0042874ccdc454a1b7a5c035
Darren Matis-Mei <dmatismei@microsoft.com>
2022-08-22 13:48:23 -0700

    Completed Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,47 +5,74 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBarWrapper = styled.div`
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  width: 370px;
-  height: auto;
-  position: relative;
-  border-radius: 4px;
-`;
-
-const ProgressBarInner = styled.div`
-  background-color: ${COLORS.primary};
-  height: var(--height);
-  padding: var(--padding);
-  position: relative;
-  width: ${p => (p.value/100) * 370}px;
-  border-radius: var(--borderRadius) ${p => (p.value >= 98 ? `var(--borderRadius)` : 0)} ${p => (p.value >= 98 ? `var(--borderRadius)` : 0)} var(--borderRadius);
-
-`;
-
 const SIZES = {
   small: {
-    "--height": 8 + "px",
-    "--borderRadius": 4 + "px",
+    "height": 8,
+    "radius": 4,
   },
   medium: {
-    "--height": 12 + "px",
-    "--borderRadius": 4 + "px",
+    "height": 12,
+    "radius": 4,
   },
   large: {
-    "--height": 24 + "px",
-    "--borderRadius": 8 + "px",
-    "--padding": 4 + "px"
+    "height": 24,
+    "radius": 8,
+    "padding": 4,
   }
 };
 
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  border-radius: var(--radius);
+`;
+
+const BarWrapper = styled.div`
+  /* This is allows us to curve the inner progress bar to fit inside the wrapper when we reach 100% */
+  overflow: hidden;
+  border-radius: 4px;
+`;
+
 const ProgressBar = ({ value, size }) => {
   const styles = SIZES[size];
-  console.log({styles});
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to Progressbar: ${size}`);
+  }
+
   return (
-    <ProgressBarWrapper style={styles}>
-      <ProgressBarInner value={value} style={styles} />
-    </ProgressBarWrapper>
+    <Wrapper
+      role="progressbar"
+      aria-aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px'
+      }}
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            "--width": value+"%",
+            "--height": styles.height + "px",
+            '--radius': styles.radius + 'px'
+          }}
+        />
+      </BarWrapper>
+    </Wrapper>
+    // <ProgressBarWrapper style={styles}>
+    //   <ProgressBarInner value={value} style={styles} />
+    // </ProgressBarWrapper>
   );
 };
 
